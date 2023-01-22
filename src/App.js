3a7f4e787f036390d2bfdc165ea132999e7e5a7b
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-22 15:06:08 +0530

    NavBar logic moved to NavBar component and added the same

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./images/logo.png";
 import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="whole-container">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
