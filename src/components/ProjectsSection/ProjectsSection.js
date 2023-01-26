0c01d97d0d898c7071a3547024e30d49b5ca709f
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-26 13:37:25 +0530

    Created ProjectsSection.js

---
+++ src/components/ProjectsSection/ProjectsSection.js
@@ -0,0 +1,7 @@
+import "./ProjectsSection.css";
+import React from "react";
+import GridWrapper from "../../UI/GridWrapper/GridWrapper";
+
+export default function ProjectsSection() {
+  return <GridWrapper>ProjectsSection</GridWrapper>;
+}
