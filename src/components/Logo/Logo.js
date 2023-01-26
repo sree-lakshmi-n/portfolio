8e0306b6a903b6b94d76be8b575d767574c23607
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-26 10:28:55 +0530

    Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Logo.css";
+import logo from "../../images/logo.png";
+
+export default function Logo() {
+  return (
+    <a href="#top" className="logo-wrapper">
+      <img src={logo} alt="company logo" className="logo" />
+    </a>
+  );
+}
