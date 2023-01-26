59516d32753e002e89715210bbaae47c6f4b94bc
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-26 11:02:51 +0530

    NavBar made Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import NavBar from "./components/NavBar/NavBar";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="whole-container">
-      <NavBar />
+    <div>
+      <Header />
     </div>
   );
 }
