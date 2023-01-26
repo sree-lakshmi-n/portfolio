16414b91bb9b5ea2efd99a18093470c2dfd1aaf1
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-26 13:36:58 +0530

    added ProjectsSection component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import HeroSection from "./components/HeroSection/HeroSection";
 import React from "react";
+import ProjectsSection from "./components/ProjectsSection/ProjectsSection";
 
 function App() {
   return (
     <div>
       <Header />
       <HeroSection />
+      <ProjectsSection />
     </div>
   );
 }
