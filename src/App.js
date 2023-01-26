5eb20924d87111400ce28e86510e7602e9fd82ce
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-26 12:53:25 +0530

    HeroSection component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import HeroSection from "./components/HeroSection/HeroSection";
+import React from "react";
 
 function App() {
   return (
     <div>
       <Header />
+      <HeroSection />
     </div>
   );
 }
