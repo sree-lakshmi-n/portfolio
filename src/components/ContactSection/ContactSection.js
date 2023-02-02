f2eb6574f8ccfdbd9e3d03221320d76a99fb7fc9
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-02-03 00:21:12 +0530

    ContactSection component

---
+++ src/components/ContactSection/ContactSection.js
@@ -0,0 +1,13 @@
+import "./ContactSection.css";
+import React from "react";
+import Logo from "../Logo/Logo";
+
+export default function ContactSection() {
+  return (
+    <section id="contact" className="section-contact ">
+      <div className="contact-container container">
+        <Logo />
+      </div>
+    </section>
+  );
+}
