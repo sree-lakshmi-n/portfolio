30ec25d14122ae9219bfcf96c07dd794331761f6
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-02-02 17:19:39 +0530

    routes added

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -2,18 +2,22 @@ import "./HeroSection.css";
 import React from "react";
 import FlexWrapper from "../../UI/FlexWrapper/FlexWrapper";
 import darr from "../../images/down-arrows.svg";
+import { BrowserRouter } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
 
 export default function HeroSection() {
   return (
-    <section className="hero container">
-      <FlexWrapper className="hero-textbox flex-center-v">
-        <p className="title">
-          Hey, I’m Sree Lakshmi N and I love building beautiful websites
-        </p>
-        <a href="#projects" className=" btn-hero">
-          <img src={darr} alt="downward arrow" className="darr-icon" />
-        </a>
-      </FlexWrapper>
-    </section>
+    <BrowserRouter>
+      <section className="hero container">
+        <FlexWrapper className="hero-textbox flex-center-v">
+          <p className="title">
+            Hey, I’m Sree Lakshmi N and I love building beautiful websites
+          </p>
+          <Link to="#projects" className=" btn-hero" smooth>
+            <img src={darr} alt="downward arrow" className="darr-icon" />
+          </Link>
+        </FlexWrapper>
+      </section>
+    </BrowserRouter>
   );
 }
