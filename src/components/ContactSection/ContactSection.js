02ac00756479a4a07fc7c6b97073675114cbc1d9
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-02-03 01:02:44 +0530

    added SocialNav component

--- src/components/ContactSection/ContactSection.js
+++ src/components/ContactSection/ContactSection.js
@@ -1,13 +1,16 @@
 import "./ContactSection.css";
 import React from "react";
 import Logo from "../Logo/Logo";
+import SocialNav from "../SocialNav/SocialNav";
+import FlexWrapper from "../../UI/FlexWrapper/FlexWrapper";
 
 export default function ContactSection() {
   return (
     <section id="contact" className="section-contact ">
-      <div className="contact-container container">
+      <FlexWrapper className="contact-container container flex-center-v">
         <Logo />
-      </div>
+        <SocialNav />
+      </FlexWrapper>
     </section>
   );
 }
