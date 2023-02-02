89e210723700d3806421c405e34dbd44ef1f4c8d
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-02-03 00:03:36 +0530

    added git as a skill

--- src/components/SkillsSection/SkillsSection.js
+++ src/components/SkillsSection/SkillsSection.js
@@ -7,7 +7,9 @@ import sass from "../../images/skills/sass.png";
 import jquery from "../../images/skills/jquery.png";
 import node from "../../images/skills/node-js.png";
 import react from "../../images/skills/react.png";
+import git from "../../images/skills/git.png";
 import GridWrapper from "../../UI/GridWrapper/GridWrapper";
+import FlexWrapper from "../../UI/FlexWrapper/FlexWrapper";
 
 export default function SkillsSection() {
   const skills = [
@@ -39,16 +41,20 @@ export default function SkillsSection() {
       icon: react,
       name: "React JS",
     },
+    {
+      icon: git,
+      name: "Github",
+    },
   ];
   return (
     <section id="skills" className="section-skills container">
       <h2 className="heading-secondary title">My Skills</h2>
-      <GridWrapper className="skills-wrapper">
+      <GridWrapper className="skills-wrapper grid--4-cols">
         {skills.map((skill) => (
-          <div className="skill">
+          <FlexWrapper className="skill">
             <img src={skill.icon} className="skill-icon" alt="skill icon" />
             <span className="skill-name">{skill.name} </span>
-          </div>
+          </FlexWrapper>
         ))}
       </GridWrapper>
     </section>
